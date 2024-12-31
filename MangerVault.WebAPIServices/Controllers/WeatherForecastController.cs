5fca7f048af8f724f97a76b37f6b709047cce359
Sanjay Kumar <imsanjayk95@gmail.com>
2025-01-01 00:24:40 +0530

    Downgrade and datetime fix

--- MangerVault.WebAPIServices/Controllers/WeatherForecastController.cs
+++ MangerVault.WebAPIServices/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace MangerVault.WebAPIServices.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateTime.Now.AddDays(index).Date,
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
